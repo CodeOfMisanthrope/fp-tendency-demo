1f386ef4e2be0fd64df66b6a1298fda5849d0277
Ilya <iosdsbb@gmail.com>
2025-01-09 15:37:28 +0300

    test: updated tests for result

--- src/lib/containers/result/spec.ts
+++ src/lib/containers/result/spec.ts
@@ -1,48 +1,78 @@
 import { Result } from "~core/containers";
 
 describe("Result", () => {
-   test("result with data", () => {
-      const data = 42;
-      const res = new Result(() => data);
+   test("should resolve with data", () => {
+      const expectedData = 42;
+      const result = new Result(() => expectedData);
 
-      res.then((val) => {
-         expect(val).toEqual(data);
+      result.then((value) => {
+         expect(value).toEqual(expectedData);
       });
    });
 
-   test("result with data catch", () => {
-      const res = new Result(() => 42);
-      let catchCalled = false;
+   test("should not trigger catch for successful result", () => {
+      const result = new Result(() => 42);
+      let catchInvoked = false;
 
-      res.catch((_) => {
-         catchCalled = true;
+      result.catch(() => {
+         catchInvoked = true;
       });
 
-      expect(catchCalled).toBe(false);
+      expect(catchInvoked).toBe(false);
    });
 
-   test("result with error", () => {
-     const error = new Error("Result error");
-      const res = new Result(() => {
+   test("should handle error correctly", () => {
+      const error = new Error("Result error occurred");
+      const result = new Result(() => {
          throw error;
       });
 
-      res.catch((err) => {
-        expect(err).toEqual(error);
+      result.catch((err) => {
+         expect(err).toEqual(error);
       });
    });
 
-   test("result with error then", () => {
-      const res = new Result(() => {
-         throw new Error("Result error");
+   test("should not trigger then for an error result", () => {
+      const result = new Result(() => {
+         throw new Error("Result error occurred");
       });
 
-      let thenCalled = false;
+      let thenInvoked = false;
 
-      res.then((_) => {
-         thenCalled = true;
+      result.then(() => {
+         thenInvoked = true;
       });
 
-      expect(thenCalled).toBe(false);
+      expect(thenInvoked).toBe(false);
+   });
+
+   test("should match correctly for successful result", () => {
+      const result = Result.Ok("Success");
+      const message = result.match(
+         (val) => `Result: ${val}`,
+         (err) => `Error: ${err.message}`,
+      );
+      expect(message).toEqual("Result: Success");
+   });
+
+   test("should match correctly for error result", () => {
+      const error = new Error("An error occurred");
+      const result = Result.Err(error);
+      const message = result.match(
+         (val) => `Result: ${val}`,
+         (err) => `Error: ${err.message}`,
+      );
+      expect(message).toEqual("Error: An error occurred");
+   });
+
+   test("should unwrap successfully", () => {
+      const result = Result.Ok("Unwrapped value");
+      expect(result.unwrap()).toEqual("Unwrapped value");
+   });
+
+   test("should unwrap error correctly", () => {
+      const error = new Error("Unwrap error");
+      const result = Result.Err(error);
+      expect(result.unwrap()).toEqual(error);
    });
 });
