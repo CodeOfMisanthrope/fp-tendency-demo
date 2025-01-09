54ff8ad8132d116e507c2c14e9e3c132dd43905d
Ilya <iosdsbb@gmail.com>
2025-01-09 15:44:48 +0300

    feat: added exports container Result, curry

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -10,9 +10,8 @@ export {
    toArray,
    toSet,
    slice,
-
 } from "~core/iter";
 
-export {curry} from "~core/utils";
+export { curry } from "~core/utils";
 
-export {Result} from "~core/containers";
+export { Result } from "~core/containers";
