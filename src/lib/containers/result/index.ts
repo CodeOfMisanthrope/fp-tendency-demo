adfffc0a48b3465b32d181468d3ae0ce52724e0f
Ilya <iosdsbb@gmail.com>
2025-01-09 15:00:30 +0300

    feat: updated Result, added unwrap, match, exec.

--- src/lib/containers/result/index.ts
+++ src/lib/containers/result/index.ts
@@ -24,6 +24,24 @@ export default class Result<T> {
       return this.state;
    }
 
+  static exec<T>(executor: () => Generator<Result<T>>) {
+    const generator = executor();
+
+    let next = generator.next();
+
+    while (!next.done) {
+      const result = next.value;
+
+      result
+        .then((value) => {
+          next = generator.next(value);
+        })
+        .catch((err) => {
+          next = generator.throw(err);
+        });
+    }
+  }
+
    constructor(executor: Resolver<T>) {
       this.state = State.IDLE;
 
@@ -36,15 +54,21 @@ export default class Result<T> {
       }
    }
 
-   public then<R>(resolver: ResolverOk<T, R>) {
-      if (this.state !== State.OK) {
-         return this;
-      }
-
+  public then<R, E>(resolverOk: ResolverOk<T, R>, resolverErr?: ResolverErr<E>) {
+    if (this.state === State.OK) {
       return new Result<R>(() => {
-         return resolver(this.val);
+        return resolverOk(this.val);
       });
-   }
+    }
+
+    if (resolverErr && this.state === State.ERR) {
+      return new Result<E>(() => {
+        return resolverErr(this.err);
+      });
+    }
+
+    return this;
+  }
 
    public catch<R>(resolver: ResolverErr<R>) {
       if (this.state !== State.ERR) {
@@ -55,4 +79,24 @@ export default class Result<T> {
          return resolver(this.err);
       });
    }
+
+  public match<R, E>(ok: ResolverOk<T, R>, err: ResolverErr<E>) {
+    if (this.state === State.OK) {
+      return ok(this.val);
+    }
+
+    if (this.state === State.ERR) {
+      return err(this.err);
+    }
+  }
+
+  public unwrap() {
+    if (this.state === State.OK) {
+      return this.val;
+    }
+
+    if (this.state === State.ERR) {
+      return this.err;
+    }
+  }
 }
