602cd1bf1e5d4268cd3a183e7c2484c65c6adda8
Ilya <iosdsbb@gmail.com>
2025-01-10 18:01:21 +0300

    chore: testing container option

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import "~assets/style/main.css";
 import esbuildApiInit from "./esbuild-api";
-import { Result } from "~core/containers";
+import { Option, Result } from "~core/containers";
 
 esbuildApiInit();
 
 console.log("Dec server is running");
 
-const res1 = new Result(() => 42);
+// const res1 = new Result(() => 42);
+// res1
+//   .then((val) => {
+//     console.log(val);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+  // .then((val) => {
+  //   console.log(val);
+  // })
+
+
+const op1 = new Option(() => 42);
+op1
+  .then((v) => {
+    console.log(v);
+    return v;
+  })
+  .catch(() => {
+    console.log(null);
+  })
